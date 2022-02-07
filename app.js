4b18d1365276bce2b481b04acc82b2c127f5ef49
sexychoco <gaeun819@naver.com>
2022-02-07 20:39:48 +0900

    erasing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const clearBtn = document.getElementById("jsClear");
 
 const CANVAS_SIZE = 700;
 const INITIAL_COLOR = "#2c2c2c";
@@ -79,6 +80,10 @@ function handleSaveClick() {
   link.click();
 }
 
+function handleClearClick() {
+  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -102,4 +107,8 @@ if (mode) {
 
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if (clearBtn) {
+  clearBtn.addEventListener("click", handleClearClick);
 }
